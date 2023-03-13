9398d3dd1ee2d20bb2e084d37b4c6c41086678ad
Arthurdw <dev@arthurdw.com>
2023-03-13 14:33:45 +0100

    :sparkles: Created a memory efficient solution for mergin two sorted linked lists

---
+++ src/merge-two-sorted-lists.rs
@@ -0,0 +1,81 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+struct Solution;
+
+impl Solution {
+    fn merge(list1: Box<ListNode>, list2: Box<ListNode>) -> Box<ListNode> {
+        let mut head = Box::new(ListNode::new(0));
+        let mut current = &mut head;
+
+        let mut list1 = Some(list1);
+        let mut list2 = Some(list2);
+
+        loop {
+            let list_val1 = list1.as_ref().unwrap();
+            let list_val2 = list2.as_ref().unwrap();
+
+            if list_val1.val < list_val2.val {
+                current.next = Some(Box::new(ListNode::new(list_val1.val)));
+                current = current.next.as_mut().unwrap();
+
+                if let Some(next) = list_val1.next.as_ref() {
+                    list1 = Some(next.clone());
+                } else {
+                    current.next = list2;
+                    break;
+                }
+            } else {
+                current.next = Some(Box::new(ListNode::new(list_val2.val)));
+                current = current.next.as_mut().unwrap();
+
+                if let Some(next) = list_val2.next.as_ref() {
+                    list2 = Some(next.clone());
+                } else {
+                    current.next = list1;
+                    break;
+                }
+            }
+
+        }
+
+        head.next.unwrap()
+    }
+
+    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match (list1, list2) {
+            (Some(li1), Some(li2)) => Some(Solution::merge(li1, li2)),
+            (Some(li1), _) => Some(li1),
+            (_, Some(li2)) => Some(li2),
+            _ => None
+        }
+    }
+}
+
+fn main() {
+    let mut list1 = Box::new(ListNode::new(1));
+    list1.next = Some(Box::new(ListNode::new(2)));
+    list1.next.as_mut().unwrap().next = Some(Box::new(ListNode::new(4)));
+
+    let mut list2 = Box::new(ListNode::new(1));
+    list2.next = Some(Box::new(ListNode::new(3)));
+    list2.next.as_mut().unwrap().next = Some(Box::new(ListNode::new(4)));
+
+    let out = Solution::merge_two_lists(Some(list1), Some(list2));
+
+    println!("{:?}", out);
+}
